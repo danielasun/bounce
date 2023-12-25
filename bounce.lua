fae389fa7c1e42f2b9fa1772c44eb9194ebd0a71
Daniel Sun <sundaniel3@gmail.com>
2023-12-25 00:03:31 +0000

    add timer for menu display add subtle panning for the ball hits

--- bounce.lua
+++ bounce.lua
@@ -14,10 +14,14 @@ ball = {
   vy=0,
 }
   
+-- global parameters
 vmax = 5
 box = {xmin=0, ymin=0, xmax=128, ymax=64}
+
+-- a reflection consists of a location (x,y) and an index. 
+-- Increase the radius of the reflection each time to make a little splash 
+-- that radiates outward and gradually fades away at each iteration.
 reflections = {}
--- a reflection consists of a location (x,y) and an index. Increase the radius of the reflection each time to make a little splash that radiates outward and is popped after 10 iterations.
 
 
 top_midi = 95
@@ -34,14 +38,17 @@ index = 1
 menu_labels = {"tape spd", "cut", "rel", "pw", "gain", "fdbk", }
 menu_items = {"delay_rate", "cutoff", "release", "pw", "gain", "delay_feedback", }
 
-params:add_control("cutoff","cutoff",controlspec.new(50,5000,'exp',0,555,'hz'))
+params:add_control("cutoff","cutoff",controlspec.new(50,10000,'exp',0,555,'hz'))
 params:set_action("cutoff", function(x) engine.cutoff(x) end)
-params:add_control("release","release",controlspec.new(0.1,10,'exp',0.01,0.3,'s'))
+params:add_control("release","release",controlspec.new(0.1,10,'exp',0.01,0.5,'s'))
 params:set_action("release", function(x) engine.release(x) end)
 params:add_control("pw","pw",controlspec.new(0.0,1.0,'lin', 0.01, 0.5,'%'))
 params:set_action("pw", function(x) engine.pw(x) end)
 params:add_control("gain","gain",controlspec.new(0.0,3.0,'lin', 0.01, 1.0,''))
 params:set_action("gain", function(x) engine.gain(x) end)
+time_of_last_menu_update = util.time()
+menu_label_timeout = 1.5 -- [s]
+pan_width = 0.5 -- number between 0 and 1 describing stereo width for the box
 
   
 engine.name = "PolyPerc"
@@ -73,21 +80,21 @@ function update()
   -- calculate bounces
   if (next_pos.x < box.xmin) then
     ball.vx = -ball.vx
-    play_tone(1)
+    play_tone(1, next_pos.x)
     add_reflection(next_pos.x, next_pos.y)
   else if(next_pos.x > box.xmax) then
     ball.vx = -ball.vx
-    play_tone(2)
+    play_tone(2, next_pos.x)
     add_reflection(next_pos.x, next_pos.y)
   end
   end
   if (next_pos.y < box.ymin) then
     ball.vy = -ball.vy
-    play_tone(3)
+    play_tone(3, next_pos.x)
     add_reflection(next_pos.x, next_pos.y)
   else if (next_pos.y > box.ymax) then
     ball.vy = -ball.vy
-    play_tone(4)
+    play_tone(4, next_pos.x)
     add_reflection(next_pos.x, next_pos.y)
   end
   end
@@ -115,11 +122,6 @@ function update()
   redraw()
 end
 
--- i=1, j=1
--- [r1, r2, nil, r3]
--- [r1, r2, r3] 
-
-
 
 function redraw()
   screen.clear()
@@ -135,12 +137,15 @@ function redraw()
   -- status
   screen.move(0, 60)
   screen.level(10)
-  screen.text(string.format("% 3d,% 3d", x, y)) 
-
-  -- parameter that you're editing
-  screen.move(125, 60)
-  screen.text_right(string.format("%s: %5.2f", menu_labels[index], params:get(menu_items[index])))
-
+  -- screen.text(string.format("% 3d,% 3d", x, y)) 
+  screen.text(string.format("%5.3f", util.time()- time_of_last_menu_update))
+  
+  -- parameter that you're editing, with menu timeout
+  if util.time() - time_of_last_menu_update <= menu_label_timeout then
+    screen.move(125, 60)
+    screen.text_right(string.format("%s: %5.2f", menu_labels[index], params:get(menu_items[index])))
+  end
+  
   -- reflections
   for i=1,#reflections do
     local r = math.floor(reflections[i].radius)
@@ -150,7 +155,7 @@ function redraw()
     
   end
   
-  
+
   screen.update()
 end
 
@@ -170,11 +175,12 @@ function enc(n, d)
     params:delta(menu_items[index], d)
   end
   end
-  
-  
+  time_of_last_menu_update = util.time()
 end  
 
-function play_tone(index)
+function play_tone(index, x)
+  local pan = (x/box.xmax)*2*pan_width - pan_width
+  engine.pan(pan)
   engine.hz(midi_to_hz(tones[index])*params:get("delay_rate"))
 end
 
